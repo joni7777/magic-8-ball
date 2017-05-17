aa6b839f6953a3a5f6938772b35eba56dddd9973
joni <jonathan_erel@adsap.co>
2017-05-17 14:01:25 -0400

    changed Magic8BallProxy to proxy

--- components/Magic8BallGame/Magic8BallGame.js
+++ components/Magic8BallGame/Magic8BallGame.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View,} from 'react-native';
+import {Text, View,} from 'react-native';
 
 import styles from './Magic8BallGame.css';
 import Magic8BallProxy from './Magic8BallGame.proxy'
